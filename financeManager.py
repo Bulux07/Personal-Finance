0934b0eedd03cb64ccc9c979672d7d0d747aa449
Jose Bulux <buluxjose22@gmail.com>
2023-08-05 19:29:07 -0400

    Updated comments for code

--- financeManager.py
+++ financeManager.py
@@ -3,9 +3,10 @@ import gspread
 import time
 #we need a timer because google only lets us pass in information every other second 
 
-#We just change this to switch from different csv files
+#An example to name the csv file to - bofa_july
+#We only change what the month equals to. This is to keep track what month we are in
 MONTH = 'july'
-#no need to change the file name it knows because month is being change
+#This fill fetch the csv file depending on the type of month you're in
 file= f"bofa_{MONTH}.CSV"
 #a list of all the transactions, dates, amount and category
 transactions = []
